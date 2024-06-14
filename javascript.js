12da2a28858153eda34acb9b69bbadb56308f8a4
ILikeJasmineRice <119762105+ILikeJasmineRixe@users.noreply.github.com>
2024-06-14 16:31:38 -0500

    Delete javascript.js

--- javascript.js
+++
@@ -1,4 +0,0 @@
-const banana_length = 7.5
-const football_field_length = 3500
-const football_banana_length = football_field_length / banana_length
-console.log(football_banana_length) // Roughly 480
