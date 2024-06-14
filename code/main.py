18625c1086463b486396727001a1db425d2657c9
ILikeJasmineRice <119762105+ILikeJasmineRixe@users.noreply.github.com>
2024-06-14 16:05:28 -0500

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,12 @@
+# Python example
+banana_length = 7.5
+football_field_length = 3500
+bananas_per_field_length = football_field_length / banana_length  # Result: 466.67
+# // roughly 480
+
+banana_width = 0.75
+football_field_width = 1920
+bananas_per_field_width = football_field_width / banana_width # Result: 2,560
+
+banana_football_area = bananas_per_field_length * bananas_per_field_width
+print(banana_football_area) # Result: 1,228,800
