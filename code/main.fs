351b65533989998cfbd04cb013f0e6b8186085bb
asshole42 <asshole@chmo.net>
2024-06-15 01:00:46 +0300

    added shitty F#

---
+++ code/main.fs
@@ -0,0 +1,10 @@
+open System
+
+let banana_width = 0.75
+let banana_length = 7.5 
+let football_width = 1920.0
+let football_length = 3600.0
+
+let bananas = (banana_width * banana_length) / (football_width * football_length)
+
+Console.WriteLine bananas
